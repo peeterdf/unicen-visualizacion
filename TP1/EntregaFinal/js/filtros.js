5b02d354b59c5182a390c5330380fefe28b506f6
Pedro di Fonzo <pedrodifonzo@gmail.com>
2017-08-24 00:01:47 -0300

    segunda parte de filtros en proceso

--- TP1/EntregaFinal/js/filtros.js
+++ TP1/EntregaFinal/js/filtros.js
@@ -7,6 +7,12 @@ var ctx2 =document.getElementById("canvas-mini").getContext("2d");
 var image1 = new Image();
 var imgRespaldo = new Image();
 
+var desdew;
+var desdeh;
+var maxw;
+var maxh;
+var hastaw;
+var hastah;
 
 function restablecerimg (image){
    myDrawImageMethod(image,ctx,document.getElementById("canvas").width,document.getElementById("canvas").height );
@@ -15,11 +21,11 @@ function cargaimg() {
   image1.src=document.getElementById("img-input").files[0].name;
   imgRespaldo.src=image1.src;
   image1.onload = function(){
-    ctx.clearRect(0,0,900,600);
+    ctx.clearRect(0,0,600,500);
    myDrawImageMethod(this,ctx,document.getElementById("canvas").width,document.getElementById("canvas").height );
  }
  imgRespaldo.onload = function(){
-   ctx2.clearRect(0,0,900,600);
+   ctx2.clearRect(0,0,600,500);
   myDrawImageMethod(this,ctx2,document.getElementById("canvas-mini").width,document.getElementById("canvas-mini").height );
   }
 }
@@ -47,18 +53,33 @@ function setPixel(imageData,x,y,r,g,b,a){
 
 function myDrawImageMethod (image, ctx,ctxwidth,ctxheight){
   if (image.width>image.height){
-    ctx.drawImage(image, 0, ((ctxheight-(ctxwidth/image.width)*image.height)/2), ctxwidth,(ctxwidth/image.width)*image.height );
+    desdew=0;
+    desdeh= Math.floor(((ctxheight-(ctxwidth/image.width)*image.height)/2))+1;
+    hastaw=ctxwidth;
+    maxw= ctxwidth;
+    hastah=Math.floor(((ctxwidth/image.width)*image.height ))+1;
+    maxh= (ctxheight-Math.floor(((ctxwidth/image.width)*image.height )))/2+Math.floor(((ctxwidth/image.width)*image.height ))+2;
+
+    //ctx.drawImage(image, 0, ((ctxheight-(ctxwidth/image.width)*image.height)/2), ctxwidth,(ctxwidth/image.width)*image.height );
   }else{
-    ctx.drawImage(image, ((ctxwidth-(ctxheight/image.height)*image.width)/2), 0, ((ctxheight/image.height)*image.width),ctxheight );
+
+    desdew=Math.floor(((ctxwidth-(ctxheight/image.height)*image.width)/2))+1;
+    desdeh= 0;
+    hastaw= Math.floor(((ctxheight/image.height)*image.width))+1;
+    maxw= ( ctxwidth-Math.floor(((ctxheight/image.height)*image.width) ))/2+ Math.floor(((ctxheight/image.height)*image.width))+1;
+    hastah=ctxheight;
+    maxh= ctxheight;
+    //ctx.drawImage(image, ((ctxwidth-(ctxheight/image.height)*image.width)/2), 0, ((ctxheight/image.height)*image.width),ctxheight );
   }
+  ctx.drawImage(image, desdew, desdeh, hastaw,hastah );
+
 }
 
 function filtroByN(){
  restablecerimg(imgRespaldo);
-
- var imageData = ctx.getImageData(0,0,c.width,image1.height);
-  for(y=0; y<image1.height; y++){
-    for(x=0; x<image1.width; x++){
+ var imageData = ctx.getImageData(0,0,c.width,c.height);
+  for(y=desdeh; y<maxh; y++){
+    for(x=desdew; x<maxw; x++){
         var pixelbyn= Math.floor(getRed(imageData,x,y)+getGreen(imageData,x,y)+getBlue(imageData,x,y)/3);
         setPixel(imageData,x,y,pixelbyn,pixelbyn,pixelbyn,255);
     }
@@ -67,21 +88,10 @@ function filtroByN(){
 }
 
 function filtroNegativo(){
- image=image1;
- imageData = ctx.getImageData(0,0,image.width,image.height);
-  for(y=0; y<image.height; y++){
-    for(x=0; x<image.width; x++){
-        setPixel(imageData,x,y,255-getRed(imageData,x,y),255-getGreen(imageData,x,y),255-getBlue(imageData,x,y),255);
-    }
-  }
-   ctx.putImageData(imageData,0,0);
-}
-
-function filtroSepia(){
- image=image1;
- imageData = ctx.getImageData(0,0,image.width,image.height);
-  for(y=0; y<image.height; y++){
-    for(x=0; x<image.width; x++){
+ restablecerimg(imgRespaldo);
+ imageData = ctx.getImageData(0,0,c.width,c.height);
+  for(y=desdeh; y<maxh; y++){
+    for(x=desdew; x<maxw; x++){
         setPixel(imageData,x,y,255-getRed(imageData,x,y),255-getGreen(imageData,x,y),255-getBlue(imageData,x,y),255);
     }
   }
@@ -89,37 +99,61 @@ function filtroSepia(){
 }
 
 function filtroBrillo(){
-  var valor=document.getElementById("brillo").value;
- image=image1;
- imageData = ctx.getImageData(0,0,image.width,image.height);
-  for(y=0; y<image.height; y++){
-    for(x=0; x<image.width; x++){
-       var pixr= getRed(imageData,x,y)+valor/2;
-       var pixg= getGreen(imageData,x,y)+valor/2;
-       var pixb= getBlue(imageData,x,y)+valor/2;
-        setPixel(imageData,x,y,pixr,pixg,pixb);
+   restablecerimg(imgRespaldo);
+  var valor= parseInt(document.getElementById("brillo").value);
+ imageData = ctx.getImageData(0,0,c.width,c.height);
+  for(y=desdeh; y<maxh; y++){
+    for(x=desdew; x<maxw; x++){
+
+       var pixr= getRed(imageData,x,y)+valor;
+       var pixg= getGreen(imageData,x,y)+valor;
+       var pixb= getBlue(imageData,x,y)+valor;
+        setPixel(imageData,x,y,pixr,pixg,pixb,255);
     }
   }
    ctx.putImageData(imageData,0,0);
 }
+
 
 function filtroBinarizacion(){
- image=image1;
- imageData = ctx.getImageData(0,0,image.width,image.height);
-  for(y=0; y<image.height; y++){
-    for(x=0; x<image.width; x++){
-        setPixel(imageData,x,y,255-getRed(imageData,x,y),255-getGreen(imageData,x,y),255-getBlue(imageData,x,y),255);
+  restablecerimg(imgRespaldo);
+var valor= parseInt(document.getElementById("bin-gradient").value);
+ imageData = ctx.getImageData(0,0,c.width,c.height);
+  for(y=desdeh; y<maxh; y++){
+    for(x=desdew; x<maxw; x++){
+      var pixr= getRed(imageData,x,y);
+      var pixg= getGreen(imageData,x,y);
+      var pixb= getBlue(imageData,x,y);
+
+      if (pixr>valor)pixr=255;
+      if (pixr<=valor)pixr=0;
+
+       setPixel(imageData,x,y,pixr,pixr,pixr,255);}
+  }
+   ctx.putImageData(imageData,0,0);
+}
+
+function filtroSepia(){
+restablecerimg(imgRespaldo);
+ imageData = ctx.getImageData(0,0,c.width,c.height);
+  for(y=desdeh; y<maxh; y++){
+    for(x=desdew; x<maxw; x++){
+      var pixr= getRed(imageData,x,y);
+      var pixg= getGreen(imageData,x,y);
+      var pixb= getBlue(imageData,x,y);
+
+      var pixsepiar=0.393*pixr + 0.769*pixg + 0.189*pixb;
+      var pixsepiag=0.349*pixr + 0.686*pixg + 0.168*pixb;
+      var pixsepiab=0.272*pixr + 0.534*pixg + 0.131*pixb;
+        setPixel(imageData,x,y,Math.floor(pixsepiar),Math.floor(pixsepiag),Math.floor(pixsepiab),255);
     }
   }
    ctx.putImageData(imageData,0,0);
 }
 
 
-
-function putImage(){
-
-  var myImage = c.toDataURL("image/png").replace("image/png", "image/octet-stream");
-  window.location.href=myImage;
-
-
-}
+var button = document.getElementById('btn-download');
+button.addEventListener('click', function (e) {
+    var dataURL = c.toDataURL('image/png');
+    button.href = dataURL;
+});
