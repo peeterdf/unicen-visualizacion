e8ade35673682ded260cab8130840d3f3ad3eed4
peeterdf <pedrodifonzo@gmail.com>
2017-08-31 01:27:38 -0300

    agario andando

---
+++ TP2/Ejercicios/js/agario.js
@@ -0,0 +1,93 @@
+//http://www.javascripture.com/
+var canvas = document.getElementById("canvas");
+var ctx =document.getElementById("canvas").getContext("2d");
+var width=900;
+var height=600;
+var imageData =ctx.createImageData(width,height);
+
+
+
+var circulos=[];
+var semillas=[];
+var circulo1 = new Circle(100,250,80,'#000000');
+//var circulo2 = new Circle(200,250,80,'#672544');
+//var circulo3 = new Circle(300,250,80,'#ac67ca');
+//var circulo4 = new Circle(400,250,80,'#97fdb2');
+circulos.push(circulo1);
+//circulos.push(circulo2);
+//circulos.push(circulo3);
+//circulos.push(circulo4);
+
+for(let i=0; i<circulos.length;i++){
+ circulos[i].dibujar(ctx);
+}
+
+
+
+function getMousePos(canvas, evt) {
+ var rect = canvas.getBoundingClientRect();
+ return {
+   x: evt.clientX - rect.left,
+   y: evt.clientY - rect.top
+ };
+}
+
+canvas.onmousedown = function(evt){
+ var mousePos = getMousePos(canvas, evt);
+
+   var isAdentro = circulos[0].estaAdentro(mousePos.x,mousePos.y);
+   if(isAdentro){
+     circulos[0].select(circulos[0].posX - mousePos.x,circulos[0].posY - mousePos.y);
+   }
+
+}
+
+canvas.onmouseup = function(evt){
+     circulos[0].unselect();
+}
+
+
+ canvas.onmousemove = function(evt) {
+ var mousePos = getMousePos(canvas, evt);
+
+ if(circulos[0].selected==1){
+   circulos[0].setX(mousePos.x+circulos[0].selx);
+   circulos[0].setY(mousePos.y+circulos[0].sely);
+   for(let i=0; i<semillas.length;i++){
+     if(semillas[i].superpone(circulos[0]) && semillas[i].comido==0){
+       if(semillas[i].color!='#000000'){
+         circulos[0].radio+=semillas[i].radio;
+         semillas[i].comido=1;
+        }else{
+          circulos[0].radio-=semillas[i].radio;
+          semillas[i].comido=1;
+        }
+     }
+   }
+ }
+   ctx.clearRect(0,0,canvas.width,canvas.height);
+   circulos[0].dibujar(ctx);
+   for(let i=0; i<semillas.length;i++){
+     semillas[i].dibujar(ctx);
+   }
+};
+
+function newSemillas(){
+   console.log('hola!');
+   var x = Math.floor(Math.random()* canvas.width);
+   var y = Math.floor(Math.random()* canvas.height);
+   var radio = 10;
+   var semilla = new Circle(x,y,radio,randomColor());
+   semillas.push(semilla);
+   semilla.dibujar(ctx);
+}
+
+function randomColor(){
+ //var color = '#'+Math.floor(Math.random()*16777215).toString(16);
+ var color = '#'; // hexadecimal starting symbol
+ var letters = ['000000','FF0000','00FF00','0000FF','FFFF00','00FFFF','FF00FF','C0C0C0']; //Set your colors here
+ color += letters[Math.floor(Math.random() * letters.length)];
+ return color;
+}
+
+setInterval(newSemillas,3000);
---
+++ TP2/Ejercicios/js/circulo.js
@@ -0,0 +1,82 @@
+function Circle(){
+  this.posX = 4;
+  this.posy = 4;
+  this.radio = 10;
+  this.color = '#141444';
+  this.selected=0;
+  this.selx=0;
+  this.sely=0;
+  this.comido=0;
+}
+
+function Circle(paramPosX, paramPosY, paramRadio, paramColor){
+  this.posX = paramPosX;
+  this.posY = paramPosY;
+  this.radio = paramRadio;
+  this.color = paramColor;
+  this.selected=0;
+  this.selx=0;
+  this.sely=0;
+  this.comido=0;
+}
+
+Circle.prototype.setX = function(x) {
+  this.posX = x;
+}
+
+Circle.prototype.setY = function(y) {
+  this.posY = y;
+}
+
+Circle.prototype.getX = function() {
+  return this.posX ;
+}
+
+Circle.prototype.getY = function() {
+  return this.posY;
+}
+
+
+Circle.prototype.message = function(){
+  alert('soy circulito con radio:'+this.radio);
+}
+
+Circle.prototype.dibujar = function(ctx){
+  if(this.comido==0){
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.posX,this.posY,this.radio,0,Math.PI * 2);
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
+Circle.prototype.estaAdentro = function(x,y){
+  var result = false;
+  var cuenta = Math.sqrt(Math.pow(x-this.posX,2) + Math.pow(y-this.posY,2));
+  if (cuenta<this.radio){
+    result = true;
+  }
+  return result;
+}
+
+Circle.prototype.select = function(x,y){
+  this.selected=1;
+  this.selx=x;
+  this.sely=y;
+}
+
+Circle.prototype.unselect = function(){
+  this.selected=0;
+  this.selx=0;
+  this.sely=0;
+}
+
+Circle.prototype.superpone = function(c){
+  var result = false;
+  var cuenta = Math.sqrt(Math.pow(c.posX-this.posX,2) + Math.pow(c.posY-this.posY,2));
+  if (cuenta+this.radio<c.radio){
+    result = true;
+  }
+  return result;
+}
